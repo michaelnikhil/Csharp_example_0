3c425ae8aeec2c03519e6b04c2da0b80ca2d43f6
Michael descamps <michaeldescamps@live.com>
2020-06-18 07:45:39 +0000

    Add project files.

---
+++ CalculAgeDLL/Age.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculAgeDLL {
+    public static class Age {
+
+        // methode qui parse la date et renvoie vrai/age is la date est ok, et renvoie faux/age=0 si la date n'est pas ok
+        public static bool calculer_age(string str_naissance, out int age) {
+            DateTime dateNaissance;
+            
+            if (!DateTime.TryParseExact(str_naissance,
+                       "dd/MM/yyyy",
+                       CultureInfo.InvariantCulture,
+                       DateTimeStyles.None,
+                       out dateNaissance)) {
+                age = 0;
+                return false;
+            } else {
+                //calcul de l'age
+                TimeSpan dt_age = DateTime.Now - dateNaissance;
+                age = (int)dt_age.TotalDays / 365;
+                return true;
+            }
+        }
+    }
+}
---
+++ michaelD/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculAgeDLL;
+
+namespace michaelD {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e) {
+            //lecture du nom et prenom
+            string nom = textNom.Text;
+            string prenom = textPrenom.Text;
+
+            //lecture de la date de naissance en string
+            string str_naissance = textNaissance.Text;
+
+            //appel de la DLL
+            int age;
+            
+            if (!  Age.calculer_age(str_naissance, out age)){
+                MessageBox.Show("Format de la date de naissance : " +
+                        "jj/mm/yyyy","Attention!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } else {
+                string reponse = "Bonjour " + prenom + " " + nom + "\n" +
+                "vous avez " + age.ToString() + " ans";
+                MessageBox.Show(reponse);
+            }
+        }
+    }
+}
